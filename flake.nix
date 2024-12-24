eb2777ef7de94ad193736e6a3fab3e71b63daaa5
toasterofbread <talohalton@gmail.com>
2024-12-24 23:48:26 +0000

    Add Android SDK to flake.nix

--- flake.nix
+++ flake.nix
@@ -1,30 +1,38 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    android-nixpkgs.url = "github:HPRIOR/android-nixpkgs/d144e1aff31d45e92ee981e04d871b000fd791f9";
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, android-nixpkgs, ... }:
     let
       system = "x86_64-linux";
 
       pkgs = import nixpkgs {
         system = system;
       };
+
+      android-sdk = (android-nixpkgs.sdk.${system} (sdkPkgs: with sdkPkgs; [
+        cmdline-tools-latest
+        platform-tools
+        build-tools-35-0-0
+        platforms-android-35
+        build-tools-34-0-0
+      ]));
     in
     {
       devShells."${system}".default =
-        let
-          pkgs = import nixpkgs {
-            system = system;
-          };
-        in
         pkgs.mkShell {
           packages = with pkgs; [
+            android-sdk
             libxcrypt-legacy
             libGL
             gtk3
             jdk17
           ];
+
+          ANDROID_HOME = "${android-sdk}/share/android-sdk";
+          GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${android-sdk}/share/android-sdk/build-tools/35.0.0/aapt2";
 
           shellHook = ''
             # Add NIX_LDFLAGS to LD_LIBRARY_PATH
