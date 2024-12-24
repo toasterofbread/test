9c5c4c5ddb605f9fef9ea37b22eb3d465d7f3415
toasterofbread <talohalton@gmail.com>
2024-12-24 23:59:45 +0000

    Remove bottom bar message

--- application/app/src/commonMain/kotlin/dev/toastbits/lifelog/application/app/ui/PersistentBottomBar.kt
+++ application/app/src/commonMain/kotlin/dev/toastbits/lifelog/application/app/ui/PersistentBottomBar.kt
@@ -12,12 +12,6 @@ import androidx.compose.ui.unit.dp
 import dev.toastbits.composekit.navigation.compositionlocal.LocalNavigator
 import dev.toastbits.composekit.navigation.navigator.Navigator
 import dev.toastbits.lifelog.application.core.ui.LinkText
-import dev.toastbits.lifelog.application.app.generated.resources.Res
-import dev.toastbits.lifelog.application.app.generated.resources.app_footer_author_prefix
-import dev.toastbits.lifelog.application.app.generated.resources.app_footer_author_suffix
-import dev.toastbits.lifelog.application.app.generated.resources.app_footer_body
-import dev.toastbits.lifelog.application.app.generated.resources.author_name
-import dev.toastbits.lifelog.application.app.generated.resources.author_url
 import org.jetbrains.compose.resources.stringResource
 
 @Composable
@@ -29,22 +23,6 @@ internal fun PersistentBottomBar(modifier: Modifier = Modifier) {
             horizontalArrangement = Arrangement.SpaceBetween,
             itemVerticalAlignment = Alignment.CenterVertically
         ) {
-            FlowRow(
-                horizontalArrangement = Arrangement.spacedBy(10.dp),
-                itemVerticalAlignment = Alignment.CenterVertically
-            ) {
-                Row {
-                    Text(stringResource(Res.string.app_footer_author_prefix), softWrap = false)
-                    LinkText(
-                        stringResource(Res.string.author_name),
-                        stringResource(Res.string.author_url),
-                        softWrap = false
-                    )
-                    Text(stringResource(Res.string.app_footer_author_suffix), softWrap = false)
-                }
-                Text(stringResource(Res.string.app_footer_body), softWrap = true)
-            }
-
             navigator.currentInfo?.also {
                 Text(it)
             }
